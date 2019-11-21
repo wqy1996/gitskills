0adbf928aa2d6d2f30518a5b043a005d5fab94b7
wqy <wqy.mail@foxmail.com>
2019-11-21 17:20:08 +0800

    woca

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 const express = require('express')
 const app = express()
-require('./util/dateFormat')
 let user = require("./router/user")
 app.use('/user', user)
-
-
-const server = app.listen(3000, function () {
+app.listen(3000, function () {
 
 })
