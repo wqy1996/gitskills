b380723cabc37d9646c839900d2a9ab18bc0a697
wqy <wqy.mail@foxmail.com>
2019-11-25 17:18:51 +0800

    搭建web服务器的啦~

---
+++ dist/projectConfig.js
@@ -0,0 +1,8 @@
+let baseURL
+// baseURL = "http://47.106.64.123:8089/"  // saas服务器
+baseURL = "http://192.168.103.215:8089/"  // 孙兵本地
+//  baseURL = "http://192.168.103.109:8081/"  // 王长飞服务器
+//  baseURL = "http://121.196.219.130:8089/"  //孙兵阿里云服务器
+// baseURL = "http://192.168.100.225:8089/"  //测试服务器
+
+module.exports = baseURL 
